cd9113eab2724e293cfe25a50f7b4276ffc811f3
mpetkov228 <mpetkov228@gmail.com>
2025-04-24 22:07:55 +0300

    Create a handler for a login command

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
 	"github.com/mpetkov228/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func handleLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("no arguments provided")
+	}
+
+	username := cmd.args[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("user %v has been set\n", username)
+
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()
