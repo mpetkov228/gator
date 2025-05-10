88a63a4cd99bfba113a02b9275f9115a3e9b3e65
mpetkov228 <mpetkov228@gmail.com>
2025-05-11 00:14:56 +0300

    Add MarkFeedFetched query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -18,3 +18,11 @@ LIMIT 1;
 -- name: GetFeeds :many
 SELECT feeds.name, feeds.url, users.name AS username FROM feeds
 INNER JOIN users ON users.id = feeds.user_id;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET 
+    last_fetched_at = NOW(),
+    updated_at = NOW()
+WHERE id = $1
+RETURNING *;
